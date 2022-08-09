6e794ec73e4cd7d336b420abf1d3d6814b4ebd69
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-08-09 20:43:31 +0530

    History Component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import List from "./List";
 
 export default function Form() {
-  const { register, handleSubmit, resetField } = useForm();
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -44,6 +45,8 @@ export default function Form() {
           </div>
         </div>
       </form>
+
+      <List></List>
     </div>
   );
 }
